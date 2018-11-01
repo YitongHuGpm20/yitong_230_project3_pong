28c7bb91634878deed5a2356b7fd2a79ba9636a9
YitongHuGpm20 <yhu216@ucsc.edu>
2018-11-01 07:42:56 -0700

    A done

--- yitong_230_project3_pong/yitong_230_project3_pong/main.cpp
+++ yitong_230_project3_pong/yitong_230_project3_pong/main.cpp
@@ -8,16 +8,14 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(1280, 800), "Yitong's Pong");
 
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
-	while (window.isOpen())
-	{
+	while (window.isOpen()){
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
+		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
